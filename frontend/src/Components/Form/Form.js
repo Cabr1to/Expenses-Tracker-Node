0f3dcf93dcaa3ae02e998954115260fa8dcb5b90
Cabr1to <luis.b.c.melo@gmail.com>
2025-01-22 11:17:40 -0300

    added /Components/Form

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from "styled-components";
 
 function From() {
-    return (
-        <div>Form</div>
-    )
+    const [inputState, setInputState] = useState({
+        title: '',
+        amount: '',
+        date: '',
+        category: '',
+        description: '',
+    })
+    const {title, amount, date, category, description} = inputState;
 
+    const handleInput = name => event => {
+        setInputState({...inputState, [name]: event.target.value});
+    }
+
+    return (
+        <FormStyled>
+            <div className="input-control">
+                <input
+                type="text"
+                value={title}
+                name={"title"}
+                placeholder={"Salary Title"}
+                onChange={handleInput('title')}
+                />
+            </div>
+        </FormStyled>
+    )
 }
 
 const FormStyled = styled.div``;
