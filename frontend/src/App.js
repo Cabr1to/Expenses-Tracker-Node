64b8e572e49eccb64f29d166593743cb5993cbfa
Cabr1to <luis.b.c.melo@gmail.com>
2025-01-22 10:09:15 -0300

    Styling front-end, added Dashboard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,33 @@
-import React  from 'react';
+import React, {useMemo} from 'react';
 import styled from "styled-components";
 import {MainLayout} from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
 import Navigation from "./Components/Navigation/Navigation";
-// import bg from '.img/bg.png'
+ import bg from './img/bg.jpg'
+
 
 function App() {
+  const[active, setActive] = React.useState(1);
+
+  const displayData = () => {
+      switch (active) {
+          case 1:
+              <Dashboard/>
+      }
+  }
+
+  const orbMemo = useMemo(() => {
+        return <Orb  />;
+  }, [])
+
   return (
-//  <AppStyled bg={bg} className="App">
-    <AppStyled className="App">
-        <Orb/>
+    <AppStyled bg={bg} className="App">
+      {orbMemo}
       <MainLayout>
-        <Navigation />
+        <Navigation active={active} setActive={setActive} />
+          <main>
+              {displayData}
+          </main>
       </MainLayout>
     </AppStyled>
   );
@@ -19,8 +35,20 @@ function App() {
 
 const AppStyled = styled.div`
     height: 100vh;
-//  background-image: url(${props => props.bg});
+    background-image: url(${props => props.bg});
     position: relative;
+    main{
+        flex: 1;
+        background: rgba(252, 246, 249, 0.78);
+        border: 3px solid #FFFFFF;
+        backdrop-filter: blur(4.5px);
+        border-radius: 32px;
+        overflow: auto;
+        overflow-x: hidden;
+        &::-webkit-scrollbar {
+            width: 0;
+        }
+    }
 `;
 
 export default App;
