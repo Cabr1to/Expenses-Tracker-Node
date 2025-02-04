d25d9550e866af2ab8efd0cc355aba463deb5019
Cabr1to <luis.b.c.melo@gmail.com>
2025-02-04 11:05:54 -0300

    added Chart component

---
+++ frontend/src/Components/Chart/Chart.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Chart as ChartJs,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+}
+
+function Chart() {
+    return (
+        <div>Chart</div>
+    )
+
+}
+export default Chart
+
+
